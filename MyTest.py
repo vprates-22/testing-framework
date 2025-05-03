e1b7cdc9a0ee23b7f8cfa787598862c1d5307f2b
vprates-22 <vprates2222@gmail.com>
2025-05-03 10:04:56 -0300

    3. Classe TestResult

--- MyTest.py
+++ MyTest.py
@@ -1,4 +1,5 @@
 from TestCase import TestCase
+from TestResult import TestResult
 
 class MyTest(TestCase):
 
@@ -17,11 +18,16 @@ class MyTest(TestCase):
     def test_c(self):
         print('test_c')
 
+
+result = TestResult()
+
 test = MyTest('test_a')
-test.run()
+test.run(result)
 
 test = MyTest('test_b')
-test.run()
+test.run(result)
 
 test = MyTest('test_c')
-test.run()
+test.run(result)
+
+print(result.summary())
--- TestCase.py
+++ TestCase.py
@@ -3,11 +3,17 @@ class TestCase:
     def __init__(self, test_method_name):
         self.test_method_name = test_method_name
 
-    def run(self):
-        self.set_up()    # chama método de setup
-        test_method = getattr(self, self.test_method_name)
-        test_method()    # chama método de teste 
-        self.tear_down() # chama método de teardown 
+    def run(self, result):
+        result.test_started()
+        self.set_up()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError as e:
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            result.add_error(self.test_method_name)
+        self.tear_down()
 
     def set_up(self):
         pass
