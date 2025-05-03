079cb3180275242a8cd5cf2e0e0603762b4ccc07
vprates-22 <vprates2222@gmail.com>
2025-05-03 10:09:59 -0300

    4. Testando TestCase

---
+++ TestStub.py
@@ -0,0 +1,12 @@
+from TestCase import TestCase
+
+class TestStub(TestCase):
+
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
